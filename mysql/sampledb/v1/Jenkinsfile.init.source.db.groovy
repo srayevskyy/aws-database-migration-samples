ff19d30c1767051f4e09d746d72bdddb8e4c35d8
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-07 20:45:01 -0800

    Updated Jenkinsfile

--- mysql/sampledb/v1/Jenkinsfile.init.source.db.groovy
+++ mysql/sampledb/v1/Jenkinsfile.init.source.db.groovy
@@ -1,10 +1,14 @@
 pipeline {
     agent any
+    environment {
+        MYSQL_ROOT_PASSWORD = credentials('MYSQL_ROOT_PASSWORD')
+    }
     stages {
         stage('Init source db') {
             steps {
                 sh 'mysql --version'
                 sh 'echo MYSQL_SRC_HOST=${MYSQL_SRC_HOST}'
+                sh 'echo MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}'
             }
         }
     }
