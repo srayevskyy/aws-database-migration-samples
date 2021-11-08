437e904a75d70f7dae85c7bdaa59c201e07b2dd7
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-07 21:01:22 -0800

    Updated Jenkinsfile

--- mysql/sampledb/v1/Jenkinsfile.init.source.db.groovy
+++ mysql/sampledb/v1/Jenkinsfile.init.source.db.groovy
@@ -6,17 +6,11 @@ pipeline {
     stages {
         stage('Init source db') {
             steps {
-                sh 'mysql --version'
-                sh 'pwd'
-                sh 'ls -al'
-                sh 'echo MYSQL_SRC_HOST=${MYSQL_SRC_HOST}'
-                sh 'echo MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}'
-                sh 'mysql -h ${MYSQL_SRC_HOST} -u root -p${MYSQL_ROOT_PASSWORD} -P3306 -e "SELECT 1+1"'
                 dir('mysql/sampledb/v1') {
                     sh "pwd"
                     sh 'ls -al'
+                    sh 'mysql -h ${MYSQL_SRC_HOST} -u root -p${MYSQL_ROOT_PASSWORD} < install-rds.sql'
                 }
-                sh 'mysql -h ${MYSQL_SRC_HOST} -u root -p${MYSQL_ROOT_PASSWORD} -P3306 -e "SELECT 1+1"'
             }
         }
     }
