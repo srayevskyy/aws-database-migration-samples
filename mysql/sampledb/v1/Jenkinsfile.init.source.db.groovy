e2d0ea21e2778e4276b885d40232d4e8f4c83187
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-16 13:18:46 -0800

    Updated jenkins job groovy script

--- mysql/sampledb/v1/Jenkinsfile.init.source.db.groovy
+++ mysql/sampledb/v1/Jenkinsfile.init.source.db.groovy
@@ -1,6 +1,8 @@
 pipeline {
     agent any
     environment {
+        MYSQL_HOST = ${MYSQL_SRC_HOST}
+        MYSQL_PORT = 3306
         MYSQL_USER = "iam_admin"
     }    
     stages {
@@ -10,9 +12,9 @@ pipeline {
                     sh '''
                     set -x
                     wget --no-clobber --directory-prefix=${HOME} https://s3.amazonaws.com/rds-downloads/rds-ca-2019-root.pem
-                    export MYSQL_TOKEN="$(aws rds generate-db-auth-token --hostname $MYSQL_DST_HOST --port 3306 --region us-west-2 --username ${MYSQL_USER})"
+                    export MYSQL_TOKEN="$(aws rds generate-db-auth-token --hostname $MYSQL_SRC_HOST --port 3306 --region us-west-2 --username ${MYSQL_USER})"
                     echo ${MYSQL_TOKEN}
-                    mysql -v -h ${MYSQL_SRC_HOST} -u ${MYSQL_USER} -p${MYSQL_TOKEN} < install-rds.sql
+                    mysql -vvv --host=${MYSQL_HOST} --port=${MYSQL_PORT} --ssl-ca=${HOME}/rds-ca-2019-root.pem --user=${MYSQL_USER} --password="${MYSQL_TOKEN}" < install-rds.sql
                     '''
                 }
             }
